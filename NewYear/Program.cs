a31a4e1126ba23ff7f37cf1942213c5b1c828d8b
Szymon Kolanowski <szymonkolanowski@op.pl>
2020-12-23 15:15:07 +0100

    adas

--- NewYear/Program.cs
+++ NewYear/Program.cs
@@ -7,7 +7,7 @@ namespace NewYear
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Happy New Year 2021");
-			Console.WriteLine("Thank you");
+			Console.WriteLine("Thank you man");
 		}
 	}
 }
